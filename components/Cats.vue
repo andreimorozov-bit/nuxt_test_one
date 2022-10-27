b5b266530285f6c0386b5fc72a851114bafdb96d
andrei <a@hz.com>
2022-10-27 11:00:59 +0300

    hz

--- components/Cats.vue
+++ components/Cats.vue
@@ -4,6 +4,34 @@
     class="pt-[64px] pb-[64px] px-[8px] min-h-[800px] max-w-[1200px] mx-auto"
   >
     <div class="text-[32px] font-bold text-amber-500">Cats</div>
+    <div class="flex flex-col md:flex-row gap-x-[32px] mt-[40px]">
+      <div class="flex flex-col w-full md:w-1/2">
+        <div class="text-[24px] font-bold text-amber-500">
+          Picture removal detract earnest is by
+        </div>
+        <div class="mt-[24px] text-[18px] text-sky-500">
+          Esteems met joy attempt way clothes yet demesne tedious. Replying an
+          marianne do it an entrance advanced. Two dare say play when hold.
+          Required bringing me material stanhill jointure is as he. Mutual
+          indeed yet her living result matter him bed whence.
+        </div>
+      </div>
+      <div class="flex flex-col w-full md:w-1/2 mt-[32px] md:mt-0">
+        <div class="flex">
+          <div class="text-amber-500"><IconHive class="text-green-500" /></div>
+          <div class="flex flex-col ml-[16px]">
+            <div class="text-[20px] font-semibold text-amber-500">
+              Instrument cultivated
+            </div>
+            <div class="mt-[12px] text-[16px] text-sky-500">
+              Far stairs now coming bed oppose hunted become his. You zealously
+              departure had procuring suspicion. Books whose front would purse
+              if be do decay.
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
